c2744e6e05137a9e3ec9b636e9e1e956229179a6
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-02 14:32:06 -0700

    add top navbar to app.js and put topic list and fav badge as child components in it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PhotoList from "components/PhotoList";
-import TopicList from "components/TopicList";
+import TopNavigation from "components/TopNavigationBar";
+// import TopicList from "components/TopicList";
 // import TopicListItem from "components/TopicListItem";
 // import PhotoListItem from "./components/PhotoListItem";
 import "./App.scss";
@@ -12,7 +13,7 @@ const App = () => {
       {/* {Array.from(Array(3)).map((_, index) => (
         <PhotoListItem key={index} photo={sampleDataForPhotoListItem} />
       ))} */}
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
