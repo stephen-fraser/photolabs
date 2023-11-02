5039b2e265b7083971a373f6a98ed578294ea2de
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-01 20:38:55 -0700

    add favicon click functionality

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,15 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+  const { handleFav, selected } = props;
+
   return (
-    <div className="photo-list__fav-icon">
+    <div onClick={handleFav} className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={selected} />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,42 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
+const PhotoListItem = (props) => {
+  const [selected, setSelected] = useState(false);
+  const handleFav = () => setSelected(selected === false ? true : false);
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+  const addPhotos = (numPhotoItems) => {
+    const photosArray = [];
 
-const PhotoListItem = () => {
-  /* Insert React */
+    for (let i = 0; i < numPhotoItems; i++) {
+      photosArray.push(
+        <div key={i} className="photo-list__item">
+          <PhotoFavButton handleFav={handleFav} selected={selected} />
+          <img
+            className="photo-list__image"
+            src={props.photo.imageSource}
+          ></img>
+
+          <div className="photo-list__user-details">
+            <img
+              className="photo-list__user-profile"
+              src={props.photo.profile}
+            ></img>
+            <div>
+              <p className="photo-list__user-info">{props.photo.username}</p>
+              <p className="photo-list__user-info photo-list__user-location">
+                {props.photo.location.city}, {props.photo.location.country}
+              </p>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    return photosArray;
+  };
+
+  return <div>{addPhotos(3)}</div>;
 };
 
 export default PhotoListItem;
