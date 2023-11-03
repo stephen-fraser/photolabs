6f819e7a8937fae83680ba80b7d1424f3c2733ef
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-03 16:57:05 -0700

    created useApplicationData for separation of concerns

---
+++ frontend/src/hooks/useApplicationData.js
@@ -0,0 +1,13 @@
+import { useReducer } from "react";
+
+// const initialState = {
+//   favouritePhotoIds: [],
+//   modalOpen: false,
+//   selectedPhoto: null,
+// };
+
+// const reducer = (state, action) => {
+//   switch (action.type) {}
+// }
+
+export default useApplicationData;
