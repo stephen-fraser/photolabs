c050b0d4e5af1c22413b3e9f3b5871d1168de5ee
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-10 16:50:38 -0800

    commented useApplicationData funtions and removed original helper functions page in same folder

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,6 +1,7 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
 
+// sets initial states for all states react is tracking
 const initialState = {
   favs: [],
   isModalOpen: false,
@@ -10,6 +11,7 @@ const initialState = {
   selectedTopic: null,
 };
 
+// declares action types for reducer switch statements and dispatches
 const actionTypes = {
   TOGGLE_FAV: "TOGGLE_FAV",
   TOGGLE_MODAL: "TOGGLE_MODAL",
@@ -19,10 +21,12 @@ const actionTypes = {
   SET_PHOTOS_BY_TOPIC: "SET_PHOTOS_BY_TOPIC",
 };
 
+// defining reducer (for use in useReducer)
 const reducer = (state, action) => {
   switch (action.type) {
     case actionTypes.TOGGLE_FAV:
       const isFav = state.favs.includes(action.id);
+      // function to change favs - it searches the favs array for photo ids and adds or removes based on its presence
       return {
         ...state,
         favs: isFav
@@ -30,26 +34,31 @@ const reducer = (state, action) => {
           : [...state.favs, action.id],
       };
     case actionTypes.TOGGLE_MODAL:
+      // boolean for toggling modal
       return {
         ...state,
         isModalOpen: !state.isModalOpen,
       };
     case actionTypes.SET_MODAL_PHOTO_DATA:
+      // sets the modal photo data
       return {
         ...state,
         modalPhotoData: action.modalPhotoData,
       };
     case actionTypes.SET_PHOTO_DATA:
+      // sets all photo data visible on homepage
       return {
         ...state,
         photoData: action.payload,
       };
     case actionTypes.SET_TOPIC_DATA:
+      // sets all topics visible in top navigation
       return {
         ...state,
         topicData: action.payload,
       };
     case actionTypes.SET_PHOTOS_BY_TOPIC:
+      // filters view of photos based on selected topic
       return {
         ...state,
         selectedTopic: action.payload,
@@ -65,14 +74,17 @@ const reducer = (state, action) => {
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // helper function to add and remove photos from favourites
   const toggleFavs = (id) => {
     dispatch({ type: actionTypes.TOGGLE_FAV, id });
   };
 
+  // helper function to toggle the boolean modal state
   const toggleModal = () => {
     dispatch({ type: actionTypes.TOGGLE_MODAL });
   };
 
+  // helper function to send the photo object to be displayed in the modal
   const sendModalPhotoData = (photoObj) => {
     dispatch({
       type: actionTypes.SET_MODAL_PHOTO_DATA,
@@ -80,12 +92,14 @@ const useApplicationData = () => {
     });
   };
 
+  // if statement for stretch functionality to return to viewing all the photos by clicking on the PhotoLabs logo in top navigation
   const getPhotosByTopicId = (topicId) => {
     if (topicId === 0) {
       axios(`/api/photos`).then((response) =>
         dispatch({ type: actionTypes.SET_PHOTO_DATA, payload: response.data })
       );
     } else {
+      // changes the topicId state
       dispatch({
         type: actionTypes.SET_PHOTOS_BY_TOPIC,
         payload: topicId,
@@ -93,6 +107,9 @@ const useApplicationData = () => {
     }
   };
 
+  // use effect to perform side effects - in this case an axios call
+  // utilize promise all - if either topics or photos axios request fail, then the whole promise fails
+  // ensures both topics and photos load - if either fail the catch will log the error
   useEffect(() => {
     Promise.all([axios("/api/photos"), axios("/api/topics")])
       .then(([photoResponse, topicResponse]) => {
@@ -108,14 +125,18 @@ const useApplicationData = () => {
       .catch((error) => {
         console.log("Error fetching data: ", error);
       });
+    // empty array so it only triggers on inital rendering
   }, []);
 
+  // use effect to perfom side effects for axios call
+  // filters photos based on selected topic
   useEffect(() => {
     if (state.selectedTopic) {
       axios(`/api/topics/photos/${state.selectedTopic}`).then((response) =>
         dispatch({ type: actionTypes.SET_PHOTO_DATA, payload: response.data })
       );
     }
+    // triggered by a changed in selected topic
   }, [state.selectedTopic]);
 
   return {
